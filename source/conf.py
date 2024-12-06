71a6190f546a6188ccee3d83e53173a29a23621e
Big Cat <xh1643406689@163.com>
2024-12-06 21:00:28 +0800

    v3

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,7 @@ release = 'v2.1.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark','sphinx_markdown_tables','sphinx_sitemap', 'sphinx_multiversion']
+extensions = ['myst_parser','sphinx_markdown_tables','sphinx_sitemap', 'sphinx_multiversion']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
